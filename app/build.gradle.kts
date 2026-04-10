14a7204b1cb8ea75ed94105e2b71dc0e5a5833d3
Takafumi Asahara <asabon.t@gmail.com>
2026-04-10 09:27:28 +0900

    警告対応

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,10 @@ plugins {
 android {
     namespace = "com.example.androidapptemplate"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version =
+            release(36) {
+                minorApiLevel = 1
+            }
     }
 
     defaultConfig {
